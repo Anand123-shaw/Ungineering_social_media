2d4a4bbfde76f03e09d5472bdef375aeb93f14dd
Anand <shaw.anand123@gmail.com>
2019-07-09 15:20:41 +0530

    homepage and homepage_loggedin php and css files

---
+++ homepage.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/homepage.css"/>
+    </head>
+    <body>
+        <div class="row row1" >
+            <div class="row row11">
+                <div class="col" >
+                    <img src="img/ungineering_logo.svg"/>
+                </div>
+                <div class="col" >
+                    <div class="row row111" >
+                        <span style="color: rgb(233,69,55)">un</span><span>gineering</span>
+                    </div>
+                    <div class="row row112" >
+                        <span>A </span><span style="color: rgb(233,69,55)">bit</span><span> of knowledge is good.A</span><span style="color: rgb(233,69,55)"> byte </span><span>is better</span>
+                    </div>
+                </div> 
+            </div>
+            <div class="row row12" >
+                <div class="col" >
+                    <p>Login</p>
+                </div>
+                <div class="col" >
+                    <p>New User</p>
+                </div>
+            </div>
+        </div>
+        <div class="row row2" >
+            <div class="row row21" >
+                <div class="row row211" >
+                <h4>Name</h4>
+                </div>
+                <div class="row row212" >
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id nisi fringilla, fermentum ligula ac, commodo sem. Sed molestie feugiat justo nec malesuada. Nulla consectetur gravida dui id vulputate.</p>
+                </div>
+                <div class="row row213" >
+                <p>Time: 24:00 Hrs IST | 26 Dec</p>
+                </div>
+            </div>
+            <div class="row row22" >
+                <div class="row row221" >
+                <h4>Name</h4>
+                </div>
+                <div class="row row222" >
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id nisi fringilla, fermentum ligula ac, commodo sem. Sed molestie feugiat justo nec malesuada. Nulla consectetur gravida dui id vulputate.</p>
+                </div>
+                <div class="row row223" >
+                <p>Time: 24:00 Hrs IST | 26 Dec</p>
+                </div>
+            </div>
+            <div class="row row23" >
+                <div class="row row231" >
+                <h4>Name</h4>
+                </div>
+                <div class="row row232" >
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id nisi fringilla, fermentum ligula ac, commodo sem. Sed molestie feugiat justo nec malesuada. Nulla consectetur gravida dui id vulputate.</p>
+                </div>
+                <div class="row row233" >
+                <p>Time: 24:00 Hrs IST | 26 Dec</p>
+                </div>
+            </div>
+        </div>
+        <div class="row row3" >
+            <div class="row row31" >
+                <p>Connect with us at</p>
+            </div>
+            <div class="row row32" >
+                <div class="col">
+                <a href="#" ><img src="img/logo-youtube-png-clipart-11.png"/></a>
+                </div>
+                <div class="col">
+                <a href="#" ><img src="img/facebook_circle_darkblue-512.png"/></a>
+                </div>
+            </div>
+            <div class="row row33" >
+                    <p>For any questions/doubts,write us on -</p>
+            </div>
+            <div class="row row34" >
+                <a href="#">queries@Ungneering.com</a>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ homepage_loggedin.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/homepage_loggedin.css"/>
+    </head>
+    <body>
+        <div class="row row1" >
+            <div class="row row11">
+                <div class="col" >
+                    <img src="img/ungineering_logo.svg"/>
+                </div>
+                <div class="col" >
+                    <div class="row row111" >
+                        <span style="color: rgb(233,69,55)">un</span><span>gineering</span>
+                    </div>
+                    <div class="row row112" >
+                        <span>A </span><span style="color: rgb(233,69,55)">bit</span><span> of knowledge is good.A</span><span style="color: rgb(233,69,55)"> byte </span><span>is better</span>
+                    </div>
+                </div> 
+            </div>
+            <div class="row row12" >
+                <div class="col" >
+                    <p>My Dashboard</p>
+                </div>
+                <div class="col" >
+                    <p>Logout</p>
+                </div>
+            </div>
+        </div>
+        <div class="row row2" >
+            <div class="row row21" >
+                <p>Write something here</p>
+            </div>
+            <div class="row row22" ></div>
+            <div class="row row23" >
+                <p>Post</p>
+            </div>
+        </div>
+        <div class="row row3" >
+            <div class="row row31" >
+                <div class="row row311" >
+                <h4>Name</h4>
+                </div>
+                <div class="row row312" >
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id nisi fringilla, fermentum ligula ac, commodo sem. Sed molestie feugiat justo nec malesuada. Nulla consectetur gravida dui id vulputate.</p>
+                </div>
+                <div class="row row313" >
+                <p>Time: 24:00 Hrs IST | 26 Dec</p>
+                </div>
+            </div>
+            <div class="row row32" >
+                <div class="row row321" >
+                <h4>Name</h4>
+                </div>
+                <div class="row row322" >
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id nisi fringilla, fermentum ligula ac, commodo sem. Sed molestie feugiat justo nec malesuada. Nulla consectetur gravida dui id vulputate.</p>
+                </div>
+                <div class="row row323" >
+                <p>Time: 24:00 Hrs IST | 26 Dec</p>
+                </div>
+            </div>
+            <div class="row row33" >
+                <div class="row row331" >
+                <h4>Name</h4>
+                </div>
+                <div class="row row332" >
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut id nisi fringilla, fermentum ligula ac, commodo sem. Sed molestie feugiat justo nec malesuada. Nulla consectetur gravida dui id vulputate.</p>
+                </div>
+                <div class="row row333" >
+                <p>Time: 24:00 Hrs IST | 26 Dec</p>
+                </div>
+            </div>
+        </div>
+        <div class="row row4" >
+            <div class="row row41" >
+                <p>Connect with us at</p>
+            </div>
+            <div class="row row42" >
+                <div class="col">
+                <a href="#" ><img src="img/logo-youtube-png-clipart-11.png"/></a>
+                </div>
+                <div class="col">
+                <a href="#" ><img src="img/facebook_circle_darkblue-512.png"/></a>
+                </div>
+            </div>
+            <div class="row row43" >
+                    <p>For any questions/doubts,write us on -</p>
+            </div>
+            <div class="row row44" >
+                <a href="#">queries@Ungneering.com</a>
+            </div>
+        </div>
+    </body>
+</html>
