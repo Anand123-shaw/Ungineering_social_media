07d2b217061b9b1f06fa2dcdb5991d31721f2b24
Anand <shaw.anand123@gmail.com>
2019-07-20 14:16:44 +0530

    homepage done

--- db.php
+++ db.php
@@ -1,12 +1,13 @@
 <?php
-    $hostname="127.0.01";
+    $hostname="127.0.0.1";
     $username="root";
     $db_password="123456";
     $db_name="social_media";
     
-    $conn=mysqli_connect($hostname,$username,$db_password,$db_name);
-    if(!$conn){
-        die("connection failed:".mysqli_connect_error());
+    $conn = mysqli_connect($hostname, $username, $db_password, $db_name);
+    if (!$conn) {
+         die("connection failed:".mysqli_connect_error());
     }
 ?>
+
     
---
+++ js/home.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('#form_submit').submit(function() {
+        var url = "homepage_loggedin_submit.php";
+        var data = $('#form_submit').serialize();
+        $.ajax({
+            url: url,
+            data: data,
+            success: submission_success,
+            error: on_error,
+            type: "POST"
+        });
+        return false;
+    });
+});
+
+var submission_success = function (response) {
+    response = JSON.parse(response);
+
+    if (response.success) {
+        //alert(response.message);
+        window.location.href = "index.php";
+    } else {
+        alert(response.message);
+    }
+};
+
+var on_error = function () {
+    alert("something went wrong");
+};
