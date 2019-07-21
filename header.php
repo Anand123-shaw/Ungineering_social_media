9a53a3770abc8674119a953f27212ea89b3e2aef
Anand <shaw.anand123@gmail.com>
2019-07-21 12:05:29 +0530

    header completely done

--- header.php
+++ header.php
@@ -1,15 +1,18 @@
-<link rel="stylesheet" href="css/common.css"/>   
+<link rel="stylesheet" href="css/common.css"/>
+
 <div class="row logo">
-    <div class="col" >
-        <a href="index.php"><img src="img/ungineering_logo.svg"/></a>
-    </div>
-    <div class="col" >
-        <div class="row tag_name" >
-            <span style="color: rgb(233,69,55)">un</span><span>gineering</span>
+    <a href="index.php">
+        <div class="col" >
+            <img src="img/ungineering_logo.svg"/>
         </div>
-        <div class="row tag_line" >
-            <span>A </span><span style="color: rgb(233,69,55)">bit</span><span> of knowledge is good.A</span><span style="color: rgb(233,69,55)"> byte </span><span>is better</span>
-        </div>
-    </div> 
+        <div class="col" >
+            <div class="row tag_name" >
+                <span style="color: rgb(233,69,55)">un</span><span>gineering</span>
+            </div>
+            <div class="row tag_line" >
+                <span>A </span><span style="color: rgb(233,69,55)">bit</span><span> of knowledge is good.A</span><span style="color: rgb(233,69,55)"> byte </span><span>is better</span>
+            </div>
+        </div> 
+    </a>
 </div>
   
